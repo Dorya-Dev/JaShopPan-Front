d80c2a76c776fe482e2942c7958dc36681d29361
Cyril Ruffa <rott@ruffa-karting.fr>
2020-10-01 12:35:19 +0200

    update Moncompte

--- src/Components/Moncompte/Moncompte.js
+++ src/Components/Moncompte/Moncompte.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./moncompte.css";
 import { Link } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
@@ -6,6 +6,60 @@ import { useHistory } from "react-router-dom";
 
 function Moncompte() {
   const history = useHistory();
+  const [tabOrder, setTabOrder] = useState("");
+
+  const handleSelect = (value) => {
+    setTabOrder(value);
+  };
+
+  function Tabselect() {
+    switch (tabOrder) {
+      case "order":
+        return (
+          <div>
+            <h2 className="title-moncompte">Vos commandes</h2>
+          </div>
+        );
+        break;
+      case "setting":
+        return (
+          <div>
+            <h2 className="title-moncompte">Paramètres de sécurité</h2>
+          </div>
+        );
+        break;
+      case "address":
+        return (
+          <div>
+            <h2 className="title-moncompte">Vos Adresses</h2>
+          </div>
+        );
+        break;
+      case "payment":
+        return (
+          <div>
+            <h2 className="title-moncompte">Vos Paiements</h2>
+          </div>
+        );
+        break;
+      case "message":
+        return (
+          <div>
+            <h2 className="title-moncompte">Votre Messagerie</h2>
+          </div>
+        );
+        break;
+      case "contact":
+        return (
+          <div>
+            <h2 className="title-moncompte">Nous Contacter</h2>
+          </div>
+        );
+        break;
+      default:
+        console.log(`Sorry, we are out of.`);
+    }
+  }
 
   function Disconnect() {
     let nom = localStorage.getItem("compte");
@@ -14,6 +68,7 @@ function Moncompte() {
       history.push("/");
     }
   }
+
   return (
     <div className="moncompte-body">
       <button className="moncompe-button" onClick={Disconnect}>
@@ -22,59 +77,95 @@ function Moncompte() {
       <br />
       <Col sm={12} className="moncompte-array">
         <Row className="mon-compte-row">
-          <Col className="mon-compte-col">
+          <Col
+            className="mon-compte-col"
+            onClick={() => {
+              handleSelect("order");
+            }}
+          >
             <img
+              id="order"
               className="simg"
               src=".\img\Moncompte\commande.png"
               alt="Vos commandes"
             />
-            Vos Commandes
+            <label htmlFor="order">Vos Commandes</label>
           </Col>
-          <Col className="mon-compte-col">
+          <Col
+            className="mon-compte-col"
+            onClick={() => {
+              handleSelect("setting");
+            }}
+          >
             <img
+              id="setting"
               className="simg"
               src=".\img\Moncompte\securite.png"
               alt="Paramètres de sécurité"
             />
-            Paramètres de sécurité
+            <label htmlFor="setting">Paramètres de sécurité</label>
           </Col>
-          <Col className="mon-compte-col">
+          <Col
+            className="mon-compte-col"
+            onClick={() => {
+              handleSelect("address");
+            }}
+          >
             <img
+              id="address"
               className="simg"
               src=".\img\Moncompte\adresse.png"
               alt="Adresse"
             />
-            Vos Adresses
+            <label htmlFor="address">Vos Adresses</label>
           </Col>
         </Row>
-
         <Row className="mon-compte-row">
-          <Col className="mon-compte-col">
+          <Col
+            className="mon-compte-col"
+            onClick={() => {
+              handleSelect("payment");
+            }}
+          >
             <img
+              id="payment"
               className="simg"
               src=".\img\Moncompte\paiement.jpg"
               alt="Vos paiements"
             />
-            Vos Paiements
+            <label htmlFor="payment">Vos Paiements</label>
           </Col>
-          <Col className="mon-compte-col">
+          <Col
+            className="mon-compte-col"
+            onClick={() => {
+              handleSelect("message");
+            }}
+          >
             <img
+              id="message"
               className="simg"
               src=".\img\Moncompte\message.png"
               alt="Messagerie"
             />
-            Votre Messagerie
+            <label htmlFor="message">Votre Messagerie</label>
           </Col>
-          <Col className="mon-compte-col">
+          <Col
+            className="mon-compte-col"
+            onClick={() => {
+              handleSelect("contact");
+            }}
+          >
             <img
+              id="contact"
               className="simg"
               src=".\img\Moncompte\contact.png"
               alt="Nous Contacter"
             />
-            Nous Contacter
+            <label htmlFor="contact">Nous Contacter</label>
           </Col>
         </Row>
       </Col>
+      <div>{Tabselect()}</div>
     </div>
   );
 }

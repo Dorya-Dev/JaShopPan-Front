b94fcfa5a6549665f69bbb7ded93d99e67f5b5b6
TheRott06 <69381950+TheRott06@users.noreply.github.com>
2020-10-29 19:16:40 +0100

    ajout poubelle /cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,14 +1,16 @@
 import userEvent from "@testing-library/user-event";
 import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import { ImCross } from "react-icons/im";
+import { MdDelete } from "react-icons/md";
 import "./cart.css";
 
 function Cart() {
   const [cart, setCart] = useState([]);
 
   const showCartProducts = cart.map((product, index) => (
-    <div key={index}>
-      <Row className="showCart">
+    <div className="showCart-content">
+      <Row key={index} className="showCart">
         <Col>
           <img
             className="product-pic"
@@ -16,10 +18,23 @@ function Cart() {
             alt="none"
           />
         </Col>
-        <Col>{product.name}</Col>
-        <Col>{product.price}</Col>
-        <Col>{product.quantity}</Col>
+        <Col>
+          <p>{product.name}</p>
+        </Col>
+
+        <Col>
+          <p>{product.price.toFixed(2)} €</p>
+        </Col>
+        <Col>
+          <p>Quantité: {product.quantity}</p>
+        </Col>
+        <Col>
+          <button onClick={() => deleteProduct(product.productId)}>
+            <MdDelete id="cart-icon" />
+          </button>
+        </Col>
       </Row>
+      <hr />
     </div>
   ));
 
@@ -49,6 +64,30 @@ function Cart() {
         }
       );
   }
+
+  const deleteProduct = (id) => {
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "bearer " + localStorage.getItem("token"),
+      },
+    };
+
+    fetch("http://localhost:4000/account/cart?productId=" + id, options)
+      .then((response) => {
+        return response.json();
+      })
+      .then(
+        (data) => {
+          alert(data.message);
+          getCart();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
 
   return <div>{showCartProducts}</div>;
 }
